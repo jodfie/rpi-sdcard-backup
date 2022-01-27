cc8dfd766e28b56b1f10e5d661b5ddb8531ce944
Jody Fielder <jodfie@gmail.com>
2022-01-27 18:18:38 -0500

    fixes thanks to tronyx

--- rpi-backup.sh
+++ rpi-backup.sh
@@ -18,11 +18,88 @@
 # but you should check that it is correct before running it.
 
 # Define some variables
-sdCardDisk=$(sudo lsblk | grep disk | awk '{print $1}')
+sdCardDisk="mmcblk0"
 sdCardMountPoint="/dev/${sdCardDisk}"
-backupDirectory='/home/pi/Desktop/'
+backupDirectory="/media/backup"
 today=$(date +%Y-%m-%d)
 days=$(( ( $(date '+%s') - $(date -d '5 weeks ago' '+%s') ) / 86400 ))
+# Colors
+readonly red='\e[31m'
+readonly endColor='\e[0m'
+
+# Script Information
+get_scriptname() {
+    local source
+    local dir
+    source="${BASH_SOURCE[0]}"
+    while [[ -L ${source} ]]; do
+        dir="$(cd -P "$(dirname "${source}")" > /dev/null && pwd)"
+        source="$(readlink "${source}")"
+        [[ ${source} != /* ]] && source="${dir}/${source}"
+    done
+    echo "${source}"
+}
+
+readonly scriptname="$(get_scriptname)"
+
+# Check whether or not user is root or used sudo
+root_check() {
+    if [[ ${EUID} -ne 0 ]]; then
+        echo -e "${red}You didn't run the script as root!${endColor}"
+        echo -e "${red}Doing it for you now...${endColor}"
+        echo ''
+        sudo bash "${scriptname:-}" "${args[@]:-}"
+        exit
+    fi
+}
+
+# Function to get PiShrink if it does not exist
+get_pishrink() {
+    piShrinkExists=$(find /usr/local/bin/ -name "*pishrink*" -type f | wc -l)
+    if [[ ${piShrinkExists} == '1' ]]; then
+        :
+    else
+        echo 'Grabbing the PiShrink script...'
+        sudo wget -O /usr/local/bin/pishrink https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh
+        sudo chmod a+x /usr/local/bin/pishrink
+    fi
+}
+
+# Function to create disk image
+create_image() {
+    echo 'Creating image file. This can take some time...'
+    sudo dd if="${sdCardMountPoint}" of="${backupDirectory}"/raspberrypi-"${today}".img bs=1M
+}
+# Fatal cd command error function
+fatal() {
+    echo 'Cannot cd to the backup directory! Does it exist?'
+    exit
+}
+# Function to check, repair, and shrink disk image
+# This shrinks my image from ~30GB to ~2.5GB so it is worth doing
+shrink_image() {
+    echo 'Shrinking image file. This can take some time...'
+    cd "${backupDirectory}" || fatal
+    sudo /usr/local/bin/pishrink -z raspberrypi-"${today}".img
+}
+
+# Function to cleanup old images from the backup directory based on the days variable specified at the top of the script
+cleanup() {
+    echo 'Removing old image files...'
+    sudo find "${backupDirectory}" -name "*.gz" -mtime +"${days}" -type f -delete
+}
+
+# Main function to run all other functions
+main() {
+    root_check
+    get_pishrink
+    create_image
+    shrink_image
+    cleanup
+}
+
+main
+e -d '5 weeks ago' '+%s') ) / 86400 ))
 # Colors
 readonly red='\e[31m'
 readonly endColor='\e[0m'
